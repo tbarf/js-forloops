a436f055dfce8d3073a4fed3d78a8faa7b98f0cc
tbarf <30032737+tbarf@users.noreply.github.com>
2017-08-15 11:58:08 -1000

    up to five done kinda

--- exercises.js
+++ exercises.js
@@ -44,12 +44,31 @@ This function will iterate through the person parameter and console.log the foll
 
 "President person was a great leader."
 */
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+for(var i = 0; i<presidents.length; i++){
+	console.log("The value at " + i + " is " +presidents[i]);
+	console.log(presidents.length);
+}
+// Can't figure out how to put person was a great leader at the end
+function leaders(person){
+	var sum = 0;
+	for(var i = 0; i<person.length; i++) {
+		return person[i];
+	}
+}
+leaders(presidents);
+console.log(presidents);
 
 
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string.
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
+var stringOfNumbers = "";
+for(var i = 10; i<=20; i++) {
+stringOfNumbers += i ;
+console.log(stringOfNumbers);
+}
 
 
 /* 5) Even Stevens
@@ -58,8 +77,13 @@ Declare a variable named `evenNumArr` and assign its value to an empty array.
 Write a for-loop that will push even numbers to the `evenNumArr` array. We want to push 50 even even numbers starting from 0.
 Console.log your results.
 */
-
-
+var evenNumArr = [];
+for (var i = 0; i<=50; i++){
+if ((i+2)%2===0) {
+        evenNumArr.push(i);
+    }
+}
+console.log(evenNumArr);
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
 
